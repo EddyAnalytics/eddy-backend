5b291f7c7858e96593a4a1e0917d0a660e7b9a94
Maurits van der Vijgh <maurits@elnino.tech>
2019-09-13 08:34:17 +0200

    Update settings to add static root so the build succeeds

--- eddy_backend/settings.py
+++ eddy_backend/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './static/'
 
 GRAPHENE = {
     'SCHEMA': 'django_root.schema.schema'
