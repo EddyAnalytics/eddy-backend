d551b349fb353355fe9038e5a258bd8ea8246fd2
Arwin Sleutjes <arwin.sleutjes@gmail.com>
2019-09-25 14:17:04 +0200

    Move dynamic_generation from experimental to utils and add extra sample graphql_code

--- workspaces/schema.py
+++ workspaces/schema.py
@@ -1,12 +1,12 @@
 import django.db.models
 
-import experimental.dynamic_generation
+import utils.dynamic_generation
 import workspaces.models
 
 model_dict = {name: cls for name, cls in workspaces.models.__dict__.items() if
               isinstance(cls, type(django.db.models.Model)) and cls.__module__ == workspaces.models.__name__}
-type__dict = {name: experimental.dynamic_generation.model_to_type_(model) for name, model in model_dict.items()}
-query_list = [experimental.dynamic_generation.model_to_query(model, type__dict[name]) for name, model in
+type__dict = {name: utils.dynamic_generation.model_to_type_(model) for name, model in model_dict.items()}
+query_list = [utils.dynamic_generation.model_to_query(model, type__dict[name]) for name, model in
               model_dict.items()]
-mutation_list = [experimental.dynamic_generation.model_to_mutation(model, type__dict[name]) for name, model in
+mutation_list = [utils.dynamic_generation.model_to_mutation(model, type__dict[name]) for name, model in
                  model_dict.items()]
