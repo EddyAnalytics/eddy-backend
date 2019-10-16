7a9c28655a39a951461da68bfb0a4892516c1eae
Arwin Sleutjes <arwin.sleutjes@gmail.com>
2019-10-16 12:00:24 +0200

    Add task routes to celery config

--- eddy_backend/celery.py
+++ eddy_backend/celery.py
@@ -3,3 +3,7 @@ from celery import Celery
 app = Celery('eddy_backend')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update({'task_routes': {
+    'app.submit_beam_sql': {'queue': 'beam'},
+    'app.submit_flink_sql': {'queue': 'flink'}
+}, })
