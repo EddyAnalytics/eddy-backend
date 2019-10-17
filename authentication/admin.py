066aa92a21dbb00f1eeffac0e8922b6e2ea4c791
Arwin Sleutjes <arwin.sleutjes@gmail.com>
2019-10-17 12:16:31 +0200

    Remove ReadOnlyIdUserAdmin

--- authentication/admin.py
+++ authentication/admin.py
@@ -4,12 +4,6 @@ from django.contrib import admin
 from authentication.models import User
 from utils.utils import ReadOnlyIdAdmin
 
-
-class ReadOnlyIdUserAdmin(ReadOnlyIdAdmin):
-    exclude = ('last_login', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined', 'groups',
-               'user_permisisons')
-
-
 admin.site.unregister(django.contrib.auth.models.User)
 admin.site.unregister(django.contrib.auth.models.Group)
-admin.site.register(User, ReadOnlyIdUserAdmin)
+admin.site.register(User, ReadOnlyIdAdmin)
