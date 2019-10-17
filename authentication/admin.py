b87be34e0bdf8aa30c27b148f277ccd1112dcc89
Arwin Sleutjes <arwin.sleutjes@gmail.com>
2019-10-17 12:22:07 +0200

    Remove unregister User

--- authentication/admin.py
+++ authentication/admin.py
@@ -4,6 +4,5 @@ from django.contrib import admin
 from authentication.models import User
 from utils.utils import ReadOnlyIdAdmin
 
-admin.site.unregister(django.contrib.auth.models.User)
 admin.site.unregister(django.contrib.auth.models.Group)
 admin.site.register(User, ReadOnlyIdAdmin)
