cc2e24500c2f3c01a87e19ec559d743d4e1d19fc
Arwin Sleutjes <arwin.sleutjes@gmail.com>
2019-10-23 14:07:43 +0200

    Change command for testing docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,35 +1,21 @@
 #!/bin/sh
 set -e
-if [ "$1" = 'makemigrations' ]; then
-  if [ $DB_TYPE = 'mysql' ]; then
-    until mysql --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USER --password=$MYSQL_PASSWORD --execute="SHOW DATABASES;" $DB_NAME; do
-      sleep 1
-    done
-    while true; do
-      sleep 1
-    done
-  else
-    while true; do
-      sleep 1
-    done
-  fi
+if [ "$1" = 'no-startup' ]; then
+  until mysql --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USER --password=$MYSQL_PASSWORD --execute="SHOW DATABASES;" $DB_NAME; do
+    sleep 1
+  done
+  while true; do
+    sleep 1
+  done
 elif [ "$1" = 'eddy-backend' ]; then
-  if [ $DB_TYPE = 'mysql' ]; then
-    until mysql --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USER --password=$MYSQL_PASSWORD --execute="SHOW DATABASES;" $DB_NAME; do
-      sleep 1
-    done
-    python3 manage.py collectstatic --no-input
-    python3 manage.py makemigrations
-    python3 manage.py migrate
-    python3 manage.py createadminuser
-    exec uwsgi --ini uwsgi.ini
-  else
-    python3 manage.py collectstatic --no-input
-    python3 manage.py makemigrations
-    python3 manage.py migrate
-    python3 manage.py createadminuser
-    exec uwsgi --ini uwsgi.ini
-  fi
+  until mysql --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USER --password=$MYSQL_PASSWORD --execute="SHOW DATABASES;" $DB_NAME; do
+    sleep 1
+  done
+  python3 manage.py collectstatic --no-input
+  python3 manage.py makemigrations
+  python3 manage.py migrate
+  python3 manage.py createadminuser
+  exec uwsgi --ini uwsgi.ini
 
   exec "$@"
 fi
