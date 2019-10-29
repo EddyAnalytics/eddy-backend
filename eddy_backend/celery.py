10d96d89094a582204d8e9c03963c973c0eb47c8
Arwin Sleutjes <arwin.sleutjes@gmail.com>
2019-10-29 11:46:38 +0100

    Add celery queue config for csv_to_kafka task

--- eddy_backend/celery.py
+++ eddy_backend/celery.py
@@ -5,5 +5,7 @@ app = Celery('eddy_backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.update({'task_routes': {
     'app.submit_beam_sql': {'queue': 'beam'},
-    'app.submit_flink_sql': {'queue': 'flink'}
+    'app.submit_flink_sql': {'queue': 'flink'},
+    'app.csv_to_kafka': {'queue': 'csv-connector'}
+
 }, })
